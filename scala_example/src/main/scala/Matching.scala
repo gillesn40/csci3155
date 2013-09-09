05fe5b8b2522543ed5846bb14803d806fb5cdf1a
Jedidiah McClurg <jrmcclurg@gmail.com>
2013-09-09 13:45:44 -0600

    Made the inorder functionality a bit more general

--- scala_example/src/main/scala/Matching.scala
+++ scala_example/src/main/scala/Matching.scala
@@ -13,6 +13,19 @@ object Matching {
         inorder(right)
     }
   }
+
+  def inorder2(tree:Tree) : List[Int] = {
+    tree match {
+      case Leaf(d) =>
+        List(d)
+      case Node(left,d,right) =>
+        inorder2(left)++
+        List(d)++
+        inorder2(right)
+    }
+  }
 }
 
 // Matching.inorder(Node(Node(Leaf(123),124,Leaf(125)),126,Leaf(127)))
+// Matching.inorder2(Node(Node(Leaf(123),124,Leaf(125)),126,Leaf(127)))
+
